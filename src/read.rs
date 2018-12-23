4fe36924fc72c418d0fa4ce43c498a80367c0646
markschl <markschl19@gmail.com>
2018-12-23 15:57:40 +0100

    Rework read.rs module
    * Interrupted errors were not correctly handled, leading to deadlocks
    * More inline statements added
    * Behavior of read() slightly changed: the number of bytes read will not 
    always be the same. Still, the code is simpler this way

--- src/read.rs
+++ src/read.rs
@@ -1,7 +1,6 @@
 //! Send data from an io::Read in a background thread to a reader in the main thread.
 
-use std::mem::replace;
-use std::io::{self, Read, Write};
+use std::io::{self, Read, Cursor};
 #[cfg(not(feature = "crossbeam_channel"))]
 use std::sync::mpsc::{channel, Receiver, Sender};
 #[cfg(feature = "crossbeam_channel")]
@@ -13,45 +12,53 @@ use crossbeam;
 #[derive(Debug)]
 struct Buffer {
     data: Box<[u8]>,
-    pos: usize,
     end: usize,
-    // read returned n = 0 -> EOF
-    eof: bool,
+    // io::ErrorKind::Interrupted
+    interrupted: bool,
 }
 
 impl Buffer {
+    #[inline]
     fn new(size: usize) -> Buffer {
         assert!(size > 0);
         Buffer {
             data: vec![0; size].into_boxed_slice(),
-            pos: 0,
             end: 0,
-            eof: false,
+            interrupted: false,
         }
     }
 
-    fn read(&mut self, mut buf: &mut [u8]) -> usize {
-        let n = buf.write(&self.data[self.pos..self.end]).unwrap();
-        self.pos += n;
-        n
-    }
-
-    /// Fill the whole buffer, unless EOF occurs. ErrorKind::Interrupted is not handled.
+    /// Fill the whole buffer
+    // if n_read > 0, then read() will called again, and EOF will only
+    // be returned then (assuming that read() will again return n == 0)
+    #[inline]
     fn refill<R: Read>(&mut self, mut reader: R) -> io::Result<()> {
+
         let mut n_read = 0;
         let mut buf = &mut *self.data;
+        self.interrupted = false;
+
         while !buf.is_empty() {
-            let n = reader.read(buf)?;
-            if n == 0 {
-                self.eof = true;
-                break
-            }
-            let tmp = buf;
-            buf = &mut tmp[n..];
-            n_read += n;
+            match reader.read(buf) {
+                Ok(n) => {
+                    if n == 0 {
+                        // EOF
+                        break;
+                    }
+                    let tmp = buf;
+                    buf = &mut tmp[n..];
+                    n_read += n;
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {
+                    self.interrupted = true;
+                    break;
+                }
+                Err(e) => return Err(e),
+            };
         }
-        self.pos = 0;
+
         self.end = n_read;
+
         Ok(())
     }
 }
@@ -61,47 +68,84 @@ impl Buffer {
 pub struct Reader {
     full_recv: Receiver<io::Result<Buffer>>,
     empty_send: Sender<Option<Buffer>>,
-    buffer: Buffer,
+    buffer: Option<Buffer>,
+    pos: usize,
 }
 
 impl Reader {
+    #[inline]
+    fn new(full_recv: Receiver<io::Result<Buffer>>, empty_send: Sender<Option<Buffer>>) -> Self {
+        Reader {
+            full_recv,
+            empty_send,
+            buffer: None,
+            pos: 0,
+        }
+    }
+
+    #[inline]
     fn done(&self) {
         self.empty_send.send(None).ok();
     }
 
-    /// return errors that may still be in the queue
+    /// return errors that may still be in the queue. This is still possible even if the
+    /// sender was dropped, as they are in the queue buffer (see docs for std::mpsc::Receiver::recv).
+    #[inline]
     fn get_errors(&self) -> io::Result<()> {
         for res in &self.full_recv {
             res?;
         }
         Ok(())
     }
+
+    // assumes that self.buffer is not None. Returns a tuple of the read result
+    // and a flag indicating if a new buffer should be received (cannot be done
+    // here due to borrow checker)
+    #[inline]
+    fn _read(&mut self, buf: &mut [u8]) -> (io::Result<usize>, bool) {
+
+        let source = self.buffer.as_mut().unwrap();
+
+        if source.interrupted && self.pos == source.end {
+            return (Err(io::Error::from(io::ErrorKind::Interrupted)), true);
+        }
+
+        let n = Cursor::new(&source.data[self.pos..source.end]).read(buf).unwrap();
+        self.pos += n;
+
+        (Ok(n), self.pos == source.end && !source.interrupted)
+    }
 }
 
 impl io::Read for Reader {
+
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        loop {
-            let n = self.buffer.read(buf);
-            if n > 0 {
-                return Ok(n);
-            } else if self.buffer.eof {
-                return Ok(0);
-            } else {
-                let data = self.full_recv.recv().ok().unwrap()?;
-                let old = replace(&mut self.buffer, data);
-                self.empty_send.send(Some(old)).ok();
-            }
+
+        if self.buffer.is_none() {
+            self.buffer = Some(self.full_recv.recv().ok().unwrap()?);
         }
+
+        let (rv, recv_next) = self._read(buf);
+
+        if recv_next {
+            self.empty_send.send(self.buffer.take()).ok();
+            self.pos = 0;
+        }
+
+        rv
     }
 }
 
 /// Wraps `reader` in a background thread and provides a reader in the main thread, which
 /// obtains data from the background reader.
 ///
-/// **Note**: Errors will not be returned immediately, but after `queuelen`
+/// The background reader fills a buffer of a given size (`bufsize`) and submits
+/// the data to the main thread through a channel. The length of its queue can
+/// be configured using the `queuelen` parameter (must be >= 1). As a consequence,
+/// errors will not be returned immediately, but after `queuelen`
 /// reads, or after reading is finished and the closure ends. The reader in the
 /// background thread will stop if an error occurrs, except for errors of kind
-/// `ErrorKind::Interrupted`. In this case, reading continues in the background,
+/// `io::ErrorKind::Interrupted`. In this case, reading continues in the background,
 /// but the error is still returned.
 ///
 /// # Example:
@@ -180,32 +224,32 @@ where
 
     let (full_send, full_recv): (Sender<io::Result<Buffer>>, _) = channel();
     let (empty_send, empty_recv): (Sender<Option<Buffer>>, _) = channel();
+
     for _ in 0..queuelen {
         empty_send.send(Some(Buffer::new(bufsize))).ok();
     }
 
     crossbeam::scope(|scope| {
         let handle = scope.spawn(move |_| {
+
             let mut reader = init_reader()?;
+
             while let Ok(Some(mut buffer)) = empty_recv.recv() {
-                if let Err(e) = buffer.refill(&mut reader) {
-                    let do_break = e.kind() != io::ErrorKind::Interrupted;
-                    full_send.send(Err(e)).ok();
-                    if do_break {
+                match buffer.refill(&mut reader) {
+                    Ok(_) => {
+                        full_send.send(Ok(buffer)).ok();
+                    }
+                    Err(e) => {
+                        full_send.send(Err(e)).ok();
                         break;
                     }
-                } else {
-                    full_send.send(Ok(buffer)).ok();
                 }
             }
+
             Ok::<_, E>(())
         });
 
-        let mut reader = Reader {
-            full_recv: full_recv,
-            empty_send: empty_send,
-            buffer: Buffer::new(bufsize),
-        };
+        let mut reader = Reader::new(full_recv, empty_send);
 
         let out = func(&mut reader)?;
 
